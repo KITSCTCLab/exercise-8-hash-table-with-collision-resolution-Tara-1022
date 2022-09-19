65f2eca642a20ec56beadab0d4c4cdf8476945b9
Tara URK21CS1022 <100012608+Tara-1022@users.noreply.github.com>
2022-09-19 14:25:24 +0530

    Second Commit

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import re
 
 def display_hash(hashtable) -> None:
-	for keyvalue in range(len(hastable)):
+	for keyvalue in range(len(hashtable)):
 		content = hashtable[keyvalue]
 		print(keyvalue, end = "")
 		for element in content:
@@ -26,4 +26,4 @@ for item in re.split('], |].', input_data):
   if len(data) > 1:
     insert(HashTable, int(data[0]), data[1])
 
-display_hash (HashTable)
+display_hash(HashTable)
